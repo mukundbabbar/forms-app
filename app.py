2606477c2eeaabc7a7583d044ce47ee7bf6f596a
mukundbabbar <mukundbabbar@gmail.com>
2021-10-01 19:31:25 +1000

    formatting

--- app.py
+++ app.py
@@ -13,32 +13,34 @@ from_number = os.environ['FROM_PHONE_NUMBER']
 
 
 def send_sms(to_number, errors):
-  print("Sending SMS")
-  try:
-      message = client.messages \
-          .create(
-               body='Congurations! Lockdown will be over soon',
-               from_=from_number,
-               status_callback=status_callback_url,
-               to=to_number
-           )
-      print(message)
-  except Exception as e:
-      errors.append("Error sending SMS")
-      print(e)
+    print("Sending SMS")
+    try:
+        message = client.messages \
+            .create(
+                body='Congurations! Lockdown will be over soon',
+                from_=from_number,
+                status_callback=status_callback_url,
+                to=to_number
+            )
+        print(message)
+    except Exception as e:
+        errors.append("Error sending SMS")
+        print(e)
+
 
 def validate(num, errors):
-  print("Validatinig Number")
-  try:
-      phone_number = client.lookups \
-                     .v1 \
-                     .phone_numbers(num) \
-                     .fetch()
-      print(phone_number)
-      return True
-  except Exception as e:
-      errors.append("Not a valid number")
-      print(e)
+    print("Validatinig Number")
+    try:
+        phone_number = client.lookups \
+            .v1 \
+            .phone_numbers(num) \
+            .fetch()
+        print(phone_number)
+        return True
+    except Exception as e:
+        errors.append("Not a valid number")
+        print(e)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -48,14 +50,14 @@ def index():
         # get url that the user has entered
         try:
             number = request.form['number']
-            # r = requests.get(number) 
+            # r = requests.get(number)
             print(number)
             to_number = number
             # validate number
             if validate(to_number, errors) == True:
-              # send SMS
-              send_sms(number, errors)
-              # Wait for delivery
+                # send SMS
+                send_sms(number, errors)
+                # Wait for delivery
         except Exception as e:
             errors.append(
                 "Unable to send text number. Please provide phone number and try again."
